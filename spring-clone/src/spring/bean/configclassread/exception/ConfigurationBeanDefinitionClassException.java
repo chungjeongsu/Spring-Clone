3f119f469c8c4328d94421a5919b30fa62b2aa78
jeongjiho <wjdwlgh2000@naver.com>
2025-12-14 19:57:51 +0900

    feat: configurationClass read 기능 완성
    - ConfigurationBeanDefinitionClassException : 현 클래스 타입 불일치 예외 추가

---
+++ spring-clone/src/spring/bean/configclassread/exception/ConfigurationBeanDefinitionClassException.java
@@ -0,0 +1,8 @@
+package spring.bean.configclassread.exception;
+
+public class ConfigurationBeanDefinitionClassException extends RuntimeException {
+
+    public ConfigurationBeanDefinitionClassException(String message) {
+        super(message);
+    }
+}
